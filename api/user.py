45847e8903ac467a46dd8e3503daf02caaa0457d
Zavgorodneva Daria <d210927@yandex.ru>
2022-07-12 13:20:00 +0300

    were fixed pep8 issues

--- api/user.py
+++ api/user.py
@@ -1,5 +1,5 @@
 #!flask/bin/python
-from flask import abort, Flask, jsonify
+from flask import abort, Flask, jsonify, request
 
 from db.users import get, get_user_by_id
 app = Flask(__name__)
@@ -8,31 +8,31 @@ app = Flask(__name__)
 # GET ../user/<string: firstname>
 @app.route('/workoutgym/api/v1.0/user/<string:name>')
 def get_user(firstname):
-    pass
+    users = get()
+    return jsonify(users)
 
 
 # GET ../user
 @app.route('/workoutgym/api/v1.0/user')
 def get_users():
     users = get()
-    if len(users) == 0:
-        abort(404)
-    return users
+    return jsonify(users)
 
-
+'''
 # POST ../user data: {firstname:, lastname:, age:, weight:, height:, birthday:}
 @app.route('/user', method=['POST'])
 def create_user():
     pass
 
-
-@app.route('/workoutgym/api/v1.0/users/<int:user_id>', methods=['GET'])
+# get
+@app.route('/workoutgym/api/v1.0/user/<int:user_id>', methods=['GET'])
 def get_users_by_id(user_id):
     users = get_user_by_id(user_id)
     if len(users) == 0:
         abort(404)
     return users
 
+'''
 
 if __name__ == '__main__':
     app.run(debug=True)
--- db/db_connection.py
+++ db/db_connection.py
@@ -1,7 +1,6 @@
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import scoped_session, sessionmaker
-import vars
 
 engine = create_engine(vars.db)
 db_session = scoped_session(sessionmaker(bind=engine))
